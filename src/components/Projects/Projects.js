5ce154e6bfa9bd250e1baa8c66c7376861310865
duytan12a7 <dinomsju@gmail.com>
2024-12-03 21:08:49 +0700

    Add Image Platformer Demo

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,6 +3,8 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import crowdrunner from "../../Assets/Projects/crowdrunner.png";
+import platformer from "../../Assets/Projects/platformer.png";
+import monstershooter from "../../Assets/Projects/monstershooter.png"
 
 function Projects() {
   return (
@@ -20,8 +22,8 @@ function Projects() {
             <ProjectCard
               imgPath={crowdrunner}
               isBlog={false}
-              title="Crowd Runner"
-              description="Đây là dự án tôi vừa làm vừa học thêm kiến thức về làm Game 3D cơ bản."
+              title="Crowd Runner 3D"
+              description="Đây là dự án tôi vừa làm vừa học thêm kiến thức về làm Game 3D."
               ghLink="https://github.com/duytan12a7/CrowdRunner"
             // demoLink=""
             />
@@ -29,11 +31,22 @@ function Projects() {
           
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={crowdrunner}
+              imgPath={platformer}
               isBlog={false}
               title="Platformer 2D"
-              description="Đây là dự án tôi vừa làm vừa học thêm kiến thức về làm Game 2D cơ bản."
+              description="Đây là dự án tôi vừa làm vừa học thêm kiến thức về làm Game 2D."
               ghLink="https://github.com/duytan12a7/Platformer_Demo"
+            // demoLink=""
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={monstershooter}
+              isBlog={false}
+              title="Monster Shooter 3D"
+              description="Đây là dự án tôi vừa làm vừa học thêm kiến thức về làm Game bắn súng 3D."
+              ghLink="https://github.com/duytan12a7/monster_shooter"
             // demoLink=""
             />
           </Col>
