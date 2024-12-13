b4429a514a15d0fb7f250824183b0707de580f3a
duytan12a7 <dinomsju@gmail.com>
2024-12-13 20:10:35 +0700

    Update Avatar

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/avatar.svg";
+// import myImg from "../../Assets/avatar.svg";
+import myImg from "../../Assets/avatar.jpg";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
@@ -60,7 +61,7 @@ function Home2() {
           </Col>
           <Col md={4} className="myAvtar">
             <Tilt>
-              <img src={myImg} className="img-fluid" alt="avatar" />
+              <img src={myImg} className="img-fluid avatar-circle" alt="avatar" />
             </Tilt>
           </Col>
         </Row>
